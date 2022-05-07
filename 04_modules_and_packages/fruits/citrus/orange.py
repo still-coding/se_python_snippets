c4f4e69ccb6307fec4fca973f1d962080d70a5b5
ivanthehairygoose <ivanthehairygoose@tuta.io>
2022-05-07 11:34:16 +0500

    finished modules and packages

---
+++ 04_modules_and_packages/fruits/citrus/orange.py
@@ -0,0 +1,6 @@
+# this file cannot be run
+
+from . import lemon
+from .. import banana
+
+print('orange')
--- 04_modules_and_packages/my_module.py
+++ 04_modules_and_packages/my_module.py
@@ -1,1 +1,9 @@
 print("i'm importing!")
+
+a = 1
+
+def b():
+    print('spam')
+
+class c:
+    pass
